e255329044798e4adc7a9fbe3ebe73b17926c3e9
namlook <nicolas.clairon@elkorado.com>
2016-06-28 17:06:59 +0200

    clean up

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,6 @@
 module Main exposing (..)
 
 import Html.App
-
-
--- import TimeTravel.Html.App as TimeTravel
-
 import Models exposing (Model, initModel)
 import Messages exposing (Msg(..))
 import Views exposing (view)
@@ -17,23 +13,14 @@ init =
     ( initModel, Cmd.map ExpensesListWidgetMsg ExpensesListWidget.Remote.fetchAllExpenses )
 
 
-
--- SUBSCRIPTIONS
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
-
--- MAIN
-
-
 main : Program Never
 main =
     Html.App.program
-        -- TimeTravel.program
         { init = init
         , view = view
         , update = update
