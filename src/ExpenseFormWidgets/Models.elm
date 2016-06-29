3a7f0131757b97920a98ee66372c7a90e002549c
namlook <nicolas.clairon@elkorado.com>
2016-06-29 16:11:41 +0200

    use toRFC3339 helper from Exts.Date

--- src/ExpenseFormWidgets/Models.elm
+++ src/ExpenseFormWidgets/Models.elm
@@ -2,7 +2,7 @@ module ExpenseFormWidgets.Models exposing (..)
 
 import String
 import Types exposing (Expense)
-import Date exposing (Date)
+import Exts.Date exposing (toRFC3339)
 
 
 type alias ExpenseForm =
@@ -43,55 +43,6 @@ initExpenseForm =
     , paidBy = ""
     , categories = ""
     }
-
-
-monthNumber : Date -> Int
-monthNumber date =
-    case (Date.month date) of
-        Date.Jan ->
-            1
-
-        Date.Feb ->
-            2
-
-        Date.Mar ->
-            3
-
-        Date.Apr ->
-            4
-
-        Date.May ->
-            5
-
-        Date.Jun ->
-            6
-
-        Date.Jul ->
-            7
-
-        Date.Aug ->
-            8
-
-        Date.Sep ->
-            9
-
-        Date.Oct ->
-            10
-
-        Date.Nov ->
-            11
-
-        Date.Dec ->
-            12
-
-
-toRFC3339 : Date -> String
-toRFC3339 date =
-    (toString <| Date.year date)
-        ++ "-"
-        ++ (String.padLeft 2 '0' <| toString <| monthNumber date)
-        ++ "-"
-        ++ (String.padLeft 2 '0' <| toString <| Date.day date)
 
 
 expense2form : Expense -> ExpenseForm
